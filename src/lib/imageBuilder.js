46d4a811e7043fe6385aaed21c51bcc6eb867ed7
BISWARANJAN GIRI <125684836+Hey-Viswa@users.noreply.github.com>
2025-06-21 16:55:10 +0000

    added hero section

---
+++ src/lib/imageBuilder.js
@@ -0,0 +1,10 @@
+import imageUrlBuilder from '@sanity/image-url';
+import client from './sanityClient';
+
+// Create image url builder
+const builder = imageUrlBuilder(client);
+
+// Helper function for generating image URLs
+export function urlFor(source) {
+  return builder.image(source);
+}
